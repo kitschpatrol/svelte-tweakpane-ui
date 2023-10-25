920b8ad8495d291f6dac61e5ecee6294e9b3a95b
Eric Mika <eric@ericmika.com>
2023-10-25 12:32:14 -0400

    Support object type value in CubicBezier.

--- src/lib/plugins/essentials/CubicBezier.svelte
+++ src/lib/plugins/essentials/CubicBezier.svelte
@@ -1,74 +1,100 @@
 <script lang="ts" context="module">
-	import type { CubicBezierBladeParams } from '@tweakpane/plugin-essentials/dist/types/cubic-bezier/plugin.d.ts';
-	export type CubicBezierValue = CubicBezierBladeParams['value'];
+	export type CubicBezierValue =
+		| {
+				x1: number;
+				y1: number;
+				x2: number;
+				y2: number;
+		  }
+		| [x1: number, y1: number, x2: number, y2: number];
 </script>
 
 <script lang="ts">
+	import type { Simplify } from 'type-fest';
+	import type { CubicBezierBladeParams as CubicBezierOptions } from '@tweakpane/plugin-essentials/dist/types/cubic-bezier/plugin.d.ts';
 	import GenericBladeFolding from '../../internal/GenericBladeFolding.svelte';
-	import type { CubicBezierApi } from '@tweakpane/plugin-essentials';
+	import type { CubicBezierApi as CubicBezierRef } from '@tweakpane/plugin-essentials';
 	import { CubicBezier } from '@tweakpane/plugin-essentials';
 	import * as pluginModule from '@tweakpane/plugin-essentials';
 	import type { ComponentProps } from 'svelte';
+	import { BROWSER } from 'esm-env';
 
-	interface $$Props
-		extends Omit<
-			ComponentProps<GenericBladeFolding<CubicBezierBladeParams, CubicBezierApi>>,
-			'ref' | 'options' | 'plugin'
-		> {
-		/** TODO Docs */
+	// TODO take an object as well as a tuple?
+	type $$Props = Omit<
+		ComponentProps<GenericBladeFolding<CubicBezierOptions, CubicBezierRef>>,
+		'ref' | 'options' | 'plugin'
+	> & {
+		/**
+		 * The cubic bezier value to control.
+		 *
+		 * Object value type is a convenience added by `svelte-tweakpane-ui`, and is not part of the original `@tweakpane/plugin-essentials` API.
+		 * @bindable
+		 */
 		value: CubicBezierValue;
-		/** TODO Docs */
+		/**
+		 * Text displayed next to the control.
+		 * @default `undefined`
+		 * */
 		label?: string;
-	}
+	};
 
-	// re-exported
-	// export let disabled: boolean = false;
-	// export let theme: Theme | undefined = undefined;
-	// export let clickToExpand: boolean = true;
-	// export let expanded: boolean | undefined = undefined;
-	// export let picker: PickerLayout | undefined = undefined;
-
-	// unique
+	// reexport for bindability
 	export let label: $$Props['label'] = undefined;
 	export let value: $$Props['value'];
-
-	// reexported for access
 	export let expanded: $$Props['expanded'] = undefined;
 
-	let options: CubicBezierBladeParams;
-	let cubicBezierBlade: CubicBezierApi;
+	let options: CubicBezierOptions;
+	let cubicBezierBlade: CubicBezierRef;
 
 	// work-arounds for funky folding
 	const buttonClass = 'tp-cbzv_b';
 
-	function getValue() {
-		return value;
+	function getValue(): CubicBezierOptions['value'] {
+		if (Array.isArray(value)) {
+			return value;
+		} else {
+			return [value.x1, value.y1, value.x2, value.y2];
+		}
 	}
 
 	function setValue() {
-		cubicBezierBlade.value = new CubicBezier(value[0], value[1], value[2], value[3]);
+		if (Array.isArray(value)) {
+			cubicBezierBlade.value = new CubicBezier(value[0], value[1], value[2], value[3]);
+		} else {
+			cubicBezierBlade.value = new CubicBezier(value.x1, value.y1, value.x2, value.y2);
+		}
 	}
 
 	function addEvent() {
 		cubicBezierBlade.on('change', (ev) => {
-			value = [ev.value.x1, ev.value.y1, ev.value.x2, ev.value.y2];
+			if (Array.isArray(value)) {
+				value = [ev.value.x1, ev.value.y1, ev.value.x2, ev.value.y2];
+			} else {
+				value = {
+					x1: ev.value.x1,
+					x2: ev.value.x2,
+					y1: ev.value.y1,
+					y2: ev.value.y2
+				};
+			}
 		});
 	}
 
-	$: options = {
-		view: 'cubicbezier',
-		label,
-		value: getValue()
-	};
-	$: cubicBezierBlade && addEvent();
-	$: value, cubicBezierBlade && setValue();
+	$: BROWSER &&
+		(options = {
+			view: 'cubicbezier',
+			label,
+			value: getValue()
+		});
+	$: BROWSER && cubicBezierBlade && addEvent();
+	$: value, BROWSER && cubicBezierBlade && setValue();
 </script>
 
 <!--
 @component
 TODO
 
-Example:
+@example
 ```tsx
 TODO
 ```
@@ -76,11 +102,13 @@ TODO
 @sourceLink
 -->
 
-<GenericBladeFolding
-	bind:expanded
-	bind:ref={cubicBezierBlade}
-	plugin={pluginModule}
-	{buttonClass}
-	{options}
-	{...$$restProps}
-/>
+{#if BROWSER}
+	<GenericBladeFolding
+		bind:expanded
+		bind:ref={cubicBezierBlade}
+		plugin={pluginModule}
+		{buttonClass}
+		{options}
+		{...$$restProps}
+	/>
+{/if}
