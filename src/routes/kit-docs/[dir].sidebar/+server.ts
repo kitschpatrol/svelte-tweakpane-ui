343704e2607fc389dc216185c631ac08bfd4befb
Eric Mika <eric@ericmika.com>
2023-10-31 15:31:35 -0400

    Group component doc into categories.

--- src/routes/kit-docs/[dir].sidebar/+server.ts
+++ src/routes/kit-docs/[dir].sidebar/+server.ts
@@ -1,9 +1,13 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { createSidebarRequestHandler } from '@svelteness/kit-docs/node';
 import type { ComponentInfo } from '$lib-docs/types/ComponentInfo';
-import { kebabCase } from 'lodash-es';
+import { kebabCase, startCase, fromPairs, sortBy, toPairs } from 'lodash-es';
 
 type SidebarLink = { title: string; slug: string };
+
+function sortRecord(record: Record<string, unknown>): Record<string, unknown> {
+	return fromPairs(sortBy(toPairs(record), ([key]) => key));
+}
 
 const components: Record<string, ComponentInfo> = structuredClone(
 	import.meta.glob('$lib-docs/generated/data/*.json', {
@@ -19,18 +23,27 @@ export async function GET(request: RequestEvent) {
 	const sidebarObject = JSON.parse(await response.text());
 
 	// add an entry for each component
-	const componentLinks: Record<string, SidebarLink[]> = {
-		Components: []
-	};
+	const componentLinks: Record<string, SidebarLink[]> = {};
 
 	Object.values(components).forEach((component) => {
-		componentLinks.Components.push({
+		if (component.pathParts.length === 0) {
+			console.warn(`Component ${component.name} has no category in its pathParts`);
+			return;
+		}
+
+		const categoryName = `${startCase(component.pathParts.at(0))} Components`;
+
+		componentLinks[categoryName] ??= [];
+		componentLinks[categoryName].push({
 			title: component.name,
 			slug: `/docs/components/${kebabCase(component.name)}`
 		});
 	});
 
-	sidebarObject.links = { ...sidebarObject.links, ...componentLinks };
+	// alphabetical happens to make sense
+	const sortedComponentLinks = sortRecord(componentLinks);
+
+	sidebarObject.links = { ...sidebarObject.links, ...sortedComponentLinks };
 
 	return new Response(JSON.stringify(sidebarObject));
 }
