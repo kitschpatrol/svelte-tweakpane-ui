fd458306d72f1a7b8cc166ae3290e52440e539b6
Eric Mika <eric@ericmika.com>
2024-05-30 20:38:33 -0400

    Add documentation on new wide slider prop.

---
+++ src/examples/docs/SvelteTweakpaneWideSlideExample.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Checkbox, IntervalSlider, Ring, Separator, Slider, Wheel } from '$lib';
+
+	let min = 0;
+	let max = 100;
+	let value = 50;
+	let wide = true;
+</script>
+
+<Checkbox bind:value={wide} label="Wide" />
+<Separator />
+<Slider bind:value {min} {max} label="Slider" {wide} />
+<IntervalSlider
+	value={[min, max]}
+	{min}
+	{max}
+	bind:meanValue={value}
+	label="IntervalSlider"
+	{wide}
+/>
+<Wheel bind:value {min} {max} label="Wheel" {wide} />
+<Ring bind:value {min} {max} label="Ring" {wide} />
