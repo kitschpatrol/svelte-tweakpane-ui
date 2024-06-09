56dc7253c9f7292cae9a9f6f83562f4a09d6eb9d
Eric Mika <eric@ericmika.com>
2024-06-09 03:17:22 -0400

    Add object nesting test example.

---
+++ src/examples/tests/TestAutoObjectNesting.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// https://github.com/kitschpatrol/svelte-tweakpane-ui/issues/10
+	import { AutoObject } from '$lib';
+
+	let object = {
+		aFolder: {
+			aSetting: 0,
+			bFolder: {
+				bSetting: 0,
+				cFolder: {
+					cSetting: 0
+				}
+			}
+		}
+	};
+</script>
+
+<AutoObject bind:object />
+
+<pre>Value: <span>{JSON.stringify(object)}</span></pre>
