d2ce4309db3705b0501e094a563ed676d931f206
Eric Mika <eric@ericmika.com>
2024-09-15 02:05:00 -0400

    Add test for issue #15.

---
+++ src/examples/tests/TestStoreInitialization.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// Via https://github.com/kitschpatrol/svelte-tweakpane-ui/issues/15
+	import { Slider } from '$lib';
+	import { onMount, tick } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const bear = writable({ apples: 100, name: 'Cleeve' });
+
+	onMount(async () => {
+		await tick();
+		$bear.apples = 60;
+	});
+</script>
+
+<p>Native input</p>
+<input bind:value={$bear.apples} min={0} max={100} step={1} type="range" />
+{$bear.apples}
+<p>Svelte Tweakpane UI</p>
+<Slider bind:value={$bear.apples} min={0} max={100} step={1} />
