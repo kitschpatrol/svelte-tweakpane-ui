12cbbd1dcba039484307313d8dd8ce18096bb78f
Eric Mika <eric@ericmika.com>
2025-03-03 17:38:26 -0500

    Knip pass.

--- knip.config.ts
+++ knip.config.ts
@@ -1,3 +1,26 @@
 import { knipConfig } from '@kitschpatrol/knip-config'
 
-export default knipConfig()
+export default knipConfig({
+	entry: ['src/examples/**/*.svelte', 'docs/src/middleware/index.ts', 'tests/**/*.ts'],
+	ignore: ['docs/src/components/**/*.{astro,mdx}'],
+	ignoreDependencies: [
+		'sharp',
+		'svelte-tweakpane-ui',
+		'@stkb/rewrap',
+		'@types/eslint',
+		'postcss-html',
+		'tslib',
+		'@prettier/plugin-php',
+		'@prettier/plugin-ruby',
+		'@prettier/plugin-xml',
+		'prettier-plugin-packagejson',
+		'prettier-plugin-sh',
+		'prettier-plugin-sql',
+		'prettier-plugin-tailwindcss',
+		'prettier-plugin-toml',
+	],
+	rules: {
+		exports: 'off',
+		types: 'off',
+	},
+})
